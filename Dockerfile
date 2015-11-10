5836c5335e6ef69414e14f943d49ac4a388a40aa
Sylvain Boily <sboily@proformatique.com>
2015-11-10 14:38:55 -0500

    example test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 from python:3.4
 
-RUN pip install websockets
+RUN pip install -r requirements.txt
 
 ADD src/ /usr/src/ws
+
+WORKDIR /usr/src/ws
--- src/main.py
+++ src/main.py
@@ -2,20 +2,67 @@
 
 import asyncio
 import datetime
-import random
 import websockets
+import logging
+import json
+import time
 
-@asyncio.coroutine
-def time(websocket, path):
-    while True:
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        if not websocket.open:
-            return
-        yield from websocket.send(now)
-        yield from asyncio.sleep(random.random() * 3)
+from multiprocessing import Process
+from multiprocessing import Queue
 
-start_server = websockets.serve(time, '0.0.0.0', 8000)
+logger = logging.getLogger(__name__)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
 
+class CoreWs(object):
+
+    def __init__(self, queue):
+        self.queue = queue
+        self.start_server = websockets.serve(self.time, '0.0.0.0', 8000)
+
+    def run(self):
+        asyncio.get_event_loop().run_until_complete(self.start_server)
+        asyncio.get_event_loop().run_forever()
+
+    @asyncio.coroutine
+    def time(self, websocket, path):
+        while True:
+            if not websocket.open:
+                return
+            yield from websocket.send(self.queue.get())
+            yield from asyncio.sleep(1)
+
+
+
+class CoreCallControl(object):
+
+    def __init__(self, queue):
+        self.queue = queue
+
+    def run(self):
+        while True:
+            message = json.dumps({'message': 'hello world'})
+            print("callcontrol:", message)
+            self.queue.put(message)
+            time.sleep(5)
+
+
+
+class Controller(object):
+
+    def __init__(self):
+        subscribeMsgQueue = Queue()
+        self.callcontrol = CoreCallControl(subscribeMsgQueue)
+        self.ws = CoreWs(subscribeMsgQueue)
+
+
+    def run(self):
+        cc_process = Process(target=self.callcontrol.run, name='callcontrol_process')
+        cc_process.start()
+
+        ws_process = Process(target=self.ws.run, name='ws_process')
+        ws_process.start()
+
+
+if __name__ == '__main__':
+    start = Controller()
+    start.run()
