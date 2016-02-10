131afde1f64cc1615d474881d2bc6358a667f982
Etienne Lessard <elessard@proformatique.com>
2016-02-10 14:25:54 -0500

    integration tests: add non matching routing key test

--- integration_tests/suite/test_bus.py
+++ integration_tests/suite/test_bus.py
@@ -5,17 +5,18 @@ import asyncio
 
 from .test_api.base import IntegrationTest
 from .test_api.constants import VALID_TOKEN_ID
+from .test_api.websocketd import WebSocketdTimeoutError
 
 
 class TestBus(IntegrationTest):
 
     asset = 'basic'
 
-    def test_bind_and_receive_message_on_xivo_exchange(self):
-        self.loop.run_until_complete(self._coro_test_bind_and_receive_message_on_xivo_exchange())
+    def test_receive_message_with_matching_routing_key(self):
+        self.loop.run_until_complete(self._coro_test_receive_message_with_matching_routing_key())
 
     @asyncio.coroutine
-    def _coro_test_bind_and_receive_message_on_xivo_exchange(self):
+    def _coro_test_receive_message_with_matching_routing_key(self):
         body = b'hello'
         yield from self.bus_client.connect()
         yield from self.bus_client.declare_xivo_exchange()
@@ -24,6 +25,25 @@ class TestBus(IntegrationTest):
         yield from self.websocketd_client.op_start()
         self.bus_client.publish_on_xivo('foo.bar', body)
 
-        received_data = yield from self.websocketd_client.recv()
+        data = yield from self.websocketd_client.recv()
 
-        self.assertEqual(received_data, body.decode('utf-8'))
+        self.assertEqual(data, body.decode('utf-8'))
+
+    def test_dont_receive_message_with_non_matching_routing_key(self):
+        self.loop.run_until_complete(self._coro_test_dont_receive_message_with_non_matching_routing_key())
+
+    @asyncio.coroutine
+    def _coro_test_dont_receive_message_with_non_matching_routing_key(self):
+        yield from self.bus_client.connect()
+        yield from self.bus_client.declare_xivo_exchange()
+        yield from self.websocketd_client.connect_and_wait_for_init(VALID_TOKEN_ID)
+        yield from self.websocketd_client.op_bind('xivo', 'foo.bar')
+        yield from self.websocketd_client.op_start()
+        self.bus_client.publish_on_xivo('foo.nomatch', b'hello')
+
+        try:
+            data = yield from self.websocketd_client.recv()
+        except WebSocketdTimeoutError:
+            pass
+        else:
+            raise AssertionError('got unexpected data from websocket: {!r}'.format(data))
