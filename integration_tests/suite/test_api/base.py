737a03d5e9091feafee7fcf797280fd4630248b8
Etienne Lessard <elessard@proformatique.com>
2016-02-10 14:15:48 -0500

    integration tests: rework loop creation/delete

--- integration_tests/suite/test_api/base.py
+++ integration_tests/suite/test_api/base.py
@@ -3,6 +3,8 @@
 
 import asyncio
 
+import asynqp
+
 from xivo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
 
 from .auth import AuthServer
@@ -24,8 +26,8 @@ class IntegrationTest(AssetLaunchingTestCase):
         #asyncio.set_event_loop(None)
 
     def setUp(self):
-        #self.loop = asyncio.new_event_loop()
         self.loop = asyncio.get_event_loop()
+        self.loop.set_exception_handler(self.__exception_handler)
         self.websocketd_client = WebSocketdClient(self.loop)
         self.auth_server = AuthServer(self.loop)
         self.bus_client = BusClient(self.loop)
@@ -33,4 +35,12 @@ class IntegrationTest(AssetLaunchingTestCase):
     def tearDown(self):
         self.loop.run_until_complete(self.websocketd_client.close())
         self.loop.run_until_complete(self.bus_client.close())
-        #self.loop.close()
+        self.loop.close()
+        asyncio.set_event_loop(asyncio.new_event_loop())
+
+    def __exception_handler(self, loop, context):
+        exception = context.get('exception')
+        if isinstance(exception, asynqp.exceptions.ConnectionClosedError):
+            print('debug: got asynqp ConnectionClosedError (happens on normal close)')
+        else:
+            loop.default_exception_handler(context)
