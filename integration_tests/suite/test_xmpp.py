fb5d8b8277d450cf8d718930d7b73703455d812e
Francois Blackburn <fblackburn@wazo.community>
2017-10-16 15:59:56 -0400

    int tests: add test when mongooseim stopped

--- integration_tests/suite/test_xmpp.py
+++ integration_tests/suite/test_xmpp.py
@@ -1,6 +1,8 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from contextlib import contextmanager
+
 from .test_api.base import IntegrationTest, run_with_loop
 from .test_api.constants import (
     VALID_TOKEN_ID,
@@ -14,3 +16,20 @@ class TestNoMongooseIM(IntegrationTest):
     @run_with_loop
     def test_no_mongooseim_server_closes_websocket(self):
         yield from self.websocketd_client.connect_and_wait_for_close(VALID_TOKEN_ID)
+
+
+class TestMongooseIMStopped(IntegrationTest):
+
+    asset = 'basic'
+
+    @run_with_loop
+    def test_mongooseim_stop_after_connected(self):
+        yield from self.websocketd_client.connect_and_wait_for_init(VALID_TOKEN_ID)
+        with self.mongooseim_stopped():
+            yield from self.websocketd_client.wait_for_close()
+
+    @contextmanager
+    def mongooseim_stopped(self):
+        self.stop_service('mongooseim', timeout=30)
+        yield
+        self.start_service('mongooseim')
