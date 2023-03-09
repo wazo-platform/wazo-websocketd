ec6b6fc4114664327851335ab438a10d4a28391d
Julien Alie <jalie@wazo.io>
2023-03-09 16:14:03 -0500

    main: remove unused logic (moved into process.py)
    This change addresses the need by:
    
    *

--- wazo_websocketd/main.py
+++ wazo_websocketd/main.py
@@ -1,19 +1,15 @@
 # Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import asyncio
 import logging
 import uvloop
-import asyncio
 
 from xivo import xivo_logging
 from xivo.user_rights import change_user
 
-from wazo_websocketd.auth import Authenticator
-from wazo_websocketd.bus import BusService
 from wazo_websocketd.config import load_config
 from wazo_websocketd.controller import Controller
-from wazo_websocketd.protocol import SessionProtocolEncoder, SessionProtocolDecoder
-from wazo_websocketd.session import SessionFactory
 
 
 def main():
@@ -29,13 +25,5 @@ def main():
     if config['user']:
         change_user(config['user'])
 
-    authenticator = Authenticator(config)
-    bus_service = BusService(config['bus'])
-    protocol_encoder = SessionProtocolEncoder()
-    protocol_decoder = SessionProtocolDecoder()
-    session_factory = SessionFactory(
-        config, authenticator, bus_service, protocol_encoder, protocol_decoder
-    )
-    controller = Controller(config, session_factory, bus_service)
-    controller.setup()
+    controller = Controller(config)
     controller.run()
