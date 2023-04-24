aa37b77b401dfc9d9c5b357262a3f688fa8615e3
Julien Alie <jalie@wazo.io>
2023-04-24 12:27:42 -0400

    processes: use `spawn` instead of `fork`
    Why:
    
    * to avoid a bug when using multiprocessing with multithreading
    where a deadlock can occur in the logging library
    
    By using spawn, a fresh interpreter is spawned instead of using a copy
    and a fresh copy of libraries are loaded (where internal locks cannot be
    held)

--- wazo_websocketd/process.py
+++ wazo_websocketd/process.py
@@ -7,11 +7,13 @@ import logging
 import websockets
 
 from logging.handlers import QueueHandler, QueueListener
-from multiprocessing import JoinableQueue, Process
+from multiprocessing import Queue, get_context
+from multiprocessing.context import SpawnProcess as Process, BaseContext
 from multiprocessing.sharedctypes import Synchronized
 from os import getpid, sched_getaffinity
 from setproctitle import setproctitle
 from signal import SIGINT, SIGTERM
+from websockets.server import Serve
 from xivo.xivo_logging import silence_loggers
 
 from .auth import Authenticator, MasterTenantProxy
@@ -24,12 +26,12 @@ logger = logging.getLogger(__name__)
 
 
 class WebsocketServer:
-    def __init__(self, config):
+    def __init__(self, config: dict):
         self._config = config
         self._tombstone = asyncio.Future()
 
-    def _create_server(self):
-        config: dict = self._config
+    def _create_server(self) -> tuple[BusService, Serve]:
+        config = self._config
         authenticator: Authenticator = Authenticator(config)
         service: BusService = BusService(config)
         factory: SessionFactory = SessionFactory(
@@ -61,33 +63,21 @@ class WebsocketServer:
         self._tombstone.set_result(True)
 
 
-class ProcessWorker(Process):
-    def __init__(self, config: dict, *sync_vars):
-        super().__init__(target=self._run_server, args=(config, *sync_vars))
-
-    def _setup_master_tenant(self, proxy: Synchronized):
-        MasterTenantProxy.proxy = proxy
-
-    def _setup_logging(self, config: dict, log_queue: JoinableQueue):
-        handler = QueueHandler(log_queue)
-        process_logger = logging.getLogger()
-        process_logger.handlers.clear()  # clear default handlers (file, stderr, etc)
-        process_logger.addHandler(handler)  # send log messages to a queue
-        process_logger.setLevel(
-            logging.DEBUG if config.get('debug') else config['log_level']
+class ProcessWorker:
+    def __init__(self, context: BaseContext, config: dict, *sync_vars):
+        self._process: Process = context.Process(
+            target=self._run_server, args=(config, *sync_vars)
         )
-        silence_loggers(['aioamqp', 'urllib3'], logging.WARNING)
+
+    def join(self):
+        self._process.join()
+
+    def start(self):
+        self._process.start()
 
     def _run_server(
-        self, config: dict, log_queue: JoinableQueue, master_tenant_proxy: Synchronized
+        self, config: dict, log_queue: Queue, master_tenant_proxy: Synchronized
     ):
-        async def serve(config):
-            loop = asyncio.get_event_loop()
-            server = WebsocketServer(config)
-            loop.add_signal_handler(SIGINT, server.stop)
-            loop.add_signal_handler(SIGTERM, server.stop)
-            await server.serve()
-
         setproctitle('wazo-websocketd: worker process')
         self._setup_logging(config, log_queue)
         self._setup_master_tenant(master_tenant_proxy)
@@ -97,15 +87,35 @@ class ProcessWorker(Process):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         try:
-            loop.run_until_complete(serve(config))
+            loop.run_until_complete(self._serve(config))
         finally:
             loop.run_until_complete(loop.shutdown_asyncgens())
             loop.close()
 
+    @staticmethod
+    async def _serve(config: dict):
+        loop = asyncio.get_event_loop()
+        server = WebsocketServer(config)
+        loop.add_signal_handler(SIGINT, server.stop)
+        loop.add_signal_handler(SIGTERM, server.stop)
+        await server.serve()
+
+    def _setup_master_tenant(self, proxy: Synchronized):
+        MasterTenantProxy.proxy = proxy
+
+    def _setup_logging(self, config: dict, log_queue: Queue):
+        handler = QueueHandler(log_queue)
+        process_logger = logging.getLogger()
+        process_logger.handlers.clear()  # clear default handlers (file, stderr, etc)
+        process_logger.addHandler(handler)  # send log messages to a queue
+        process_logger.setLevel(
+            logging.DEBUG if config.get('debug') else config['log_level']
+        )
+        silence_loggers(['aioamqp', 'urllib3'], logging.WARNING)
+
 
 class ProcessPool:
     def __init__(self, config: dict):
-        handlers = logging.getLogger().handlers
         workers: int | str = config['process_workers']
         if workers == 'auto':
             workers = len(sched_getaffinity(0))
@@ -115,21 +125,20 @@ class ProcessPool:
                 'configuration key `process_workers` must be a positive integer or `auto`'
             )
 
-        self._config: dict = config
-        self._poolsize: int = workers
-        self._log_queue = JoinableQueue()
-        self._log_listener = QueueListener(self._log_queue, *handlers)
-        self._workers: list[ProcessWorker] = []
+        context = get_context('spawn')
+        log_queue = context.JoinableQueue(10000)
+        handlers = logging.getLogger().handlers
+        self._log_listener = QueueListener(
+            log_queue, *handlers, respect_handler_level=True
+        )
 
-    def __len__(self):
-        return len(self._workers)
+        self._workers: set[ProcessWorker] = {
+            ProcessWorker(context, config, log_queue, MasterTenantProxy.proxy)
+            for _ in range(workers)
+        }
 
     async def __aenter__(self):
-        self._workers = {
-            ProcessWorker(self._config, self._log_queue, MasterTenantProxy.proxy)
-            for _ in range(self._poolsize)
-        }
-        logger.info('starting %d worker process(es)', self._poolsize)
+        logger.info('starting %d worker process(es)', len(self._workers))
         self._log_listener.start()
         for worker in self._workers:
             worker.start()
@@ -138,7 +147,5 @@ class ProcessPool:
     async def __aexit__(self, *args):
         for worker in self._workers:
             worker.join()
-            worker.close()
-        logger.info('processing queued log messages...')
-        self._log_queue.join()
+        logger.info('processing remaining log messages...')
         self._log_listener.stop()
