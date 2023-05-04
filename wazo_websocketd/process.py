9fc4cfc257f58b4c161f99eeccd5ebc688d277af
Julien Alie <jalie@wazo.io>
2023-05-04 11:52:16 -0400

    avoid permission errors when run from /root
    * Create a tempdir in /tmp to allow processes to spawn correctly
    without permission errors related to /root when run as `wazo-websocketd`

--- wazo_websocketd/process.py
+++ wazo_websocketd/process.py
@@ -8,9 +8,10 @@ import websockets
 
 from multiprocessing import get_context
 from multiprocessing.sharedctypes import Synchronized
-from os import getpid, sched_getaffinity
+from os import getpid, sched_getaffinity, chdir
 from setproctitle import setproctitle
 from signal import SIGINT, SIGTERM
+from tempfile import TemporaryDirectory
 from websockets.server import Serve
 from xivo.xivo_logging import silence_loggers, setup_logging
 
@@ -73,8 +74,10 @@ class ProcessPool:
             )
         self._workers = workers
         self._config = config
+        self._dir = TemporaryDirectory(prefix="wazo-websocketd-")
 
         context = get_context('spawn')
+        chdir(self._dir.name)
         self._pool = context.Pool(
             workers, self._init_worker, (config, MasterTenantProxy.proxy)
         )
@@ -88,6 +91,7 @@ class ProcessPool:
     async def __aexit__(self, *args):
         self._pool.close()
         self._pool.join()
+        self._dir.cleanup()
 
     @staticmethod
     def _init_worker(config: dict, master_tenant_proxy: Synchronized):
